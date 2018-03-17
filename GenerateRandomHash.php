8882150acfdafef06ad90de686f3b805280cbbd3
Sina Moradi <33667141+Moradix96@users.noreply.github.com>
2018-03-17 09:52:50 +0330

    Add files via upload

---
+++ GenerateRandomHash.php
@@ -0,0 +1,9 @@
+<?php
+/* 
+ * Generate Random Hash
+ * Sina Moradi
+ * R1 - 17/03/2018
+ */
+
+$hash = md5(rand(0,1000)); // Generate random 32 character hash and assign it to a local variable.
+// Example output: f4552671f8909587cf485ea990207f3b
